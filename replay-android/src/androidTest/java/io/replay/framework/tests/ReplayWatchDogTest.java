00df241559cd36072532d0b8d1bbd798805903e1
aric hunter <arichunter@arics-MacBook-Pro.local>
2014-09-30 10:34:53 -0700

    removed extraneous enqueueing

--- replay-android/src/androidTest/java/io/replay/framework/tests/ReplayWatchDogTest.java
+++ replay-android/src/androidTest/java/io/replay/framework/tests/ReplayWatchDogTest.java
@@ -31,25 +31,6 @@ public class ReplayWatchDogTest extends ServiceTestCase<WatchDogServiceWrapper> 
 
         latch = new CountDownLatch(1);
         getService().setLatch(latch);
-
-        Context context = getContext();
-
-        // load parameters
-        Config mConfig = ReplayParams.getOptions(context.getApplicationContext());
-        mConfig.setApiKey("testKey");
-        mConfig.setDispatchInterval(3600000); // 60 min == infinity?
-        ReplayQueue queue = new ReplayQueue(context, mConfig);
-        queue.clear();
-        queue.start();
-
-        //add events to queue
-        ReplayJsonObject json = new ReplayJsonObject();
-        json.put("event_name", "test");
-
-        queue.enqueue(new TestReplayJob(new ReplayRequest(ReplayConfig.RequestType.EVENTS, json)));
-        queue.enqueue(new TestReplayJob(new ReplayRequest(ReplayConfig.RequestType.EVENTS, json)));
-        queue.enqueue(new TestReplayJob(new ReplayRequest(ReplayConfig.RequestType.EVENTS, json)));
-        queue.stop();
     }
 
     public void testHandleIntent() throws InterruptedException {
