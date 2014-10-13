f463a851206990eb41eb2f694f950cb4ba2c1b6f
Parth <parth.padg@gmail.com>
2014-10-13 15:02:06 -0700

    WIP - setting up automatic deploys to sonatype

--- scripts/replay-envsetup.sh
+++ scripts/replay-envsetup.sh
@@ -34,4 +34,28 @@ function waitAVD {
     )
 }
 
+function sonatypePublish{
+    #http://benlimmer.com/2014/01/04/automatically-publish-to-sonatype-with-gradle-and-travis-ci/
+    #http://www.survivingwithandroid.com/2014/05/android-guide-to-publish-aar-to-maven-gradle.html
+
+    echo -e "Beginning publish task to Sonatype/MavenCentral"
+
+    echo -e $SONATYPE_PRIVATE_KEY >> privatekey.gpg
+
+    ./gradlew uploadArchives -PUSERNAME="${SONTAYPE_USERNAME}" -PPASSWORD="${SONATYPE_PASSWORD}" \
+        -Psigning.keyId=parth@originate.com -Psigning.password="${SONTAYPE_PASSWORD}" -Psigning.secretKeyRingFile=privatekey.gpg
+
+    RETVAL=$?
+
+    if [ $RETVAL -eq 0 ]; then
+        echo 'Completed publish!'
+      else
+        echo 'Publish failed.'
+        return 1
+      fi
+    else
+        echo 'Skipping Sonatype push'
+    fi
+
+}
 
