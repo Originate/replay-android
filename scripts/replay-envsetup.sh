aa0164f4cc4a21307acd9ed025fd795ca935fc9d
Parth <parth.padg@gmail.com>
2015-06-18 10:01:49 -0700

    updated dependencies

--- scripts/replay-envsetup.sh
+++ scripts/replay-envsetup.sh
@@ -9,13 +9,11 @@ function ensureDependenciesAndCreateAVD(){
 
   if [ ! -e $DEPS ]; then
     cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
-    #echo y | android update sdk -u -a -t android-8 &&
     echo y | android update sdk -u -a -t android-19 &&
     echo y | android update sdk -u -a -t platform-tools &&
-    echo y | android update sdk -u -a -t build-tools-20.0.0 &&
+    echo y | android update sdk -u -a -t build-tools-22.0.1 &&
     echo y | android update sdk -u -a -t sys-img-x86-android-19 &&
-    #echo y | android update sdk -u -a -t addon-google_apis-google-18 && 
-    echo no | android create avd -n testAVD -f -t android-19 --abi default/x86 &&
+    echo no | android create avd -n testAVD -f -t android-19 --abi default/armeabi-v7a &&
     touch $DEPS
   fi
 }
